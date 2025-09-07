cc38770b7b181f6a6bcfe32703c030306b168851
Luiz-Garcia-MSc <109922407+Luiz-Garcia-MSc@users.noreply.github.com>
2025-09-07 08:52:17 -0300

    melhoria nos evaluate_fairness e evaluate_performance

--- run_exp.py
+++ run_exp.py
@@ -9,12 +9,7 @@ from datetime import datetime
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.metrics import (
-    accuracy_score, f1_score, recall_score, precision_score,
-    roc_auc_score, average_precision_score, matthews_corrcoef,
-    balanced_accuracy_score, log_loss, confusion_matrix
-)
-from sklearn.datasets import fetch_openml
+from sklearn.metrics import (roc_auc_score, average_precision_score, matthews_corrcoef, log_loss, confusion_matrix)
 
 from aif360.datasets import BinaryLabelDataset
 from aif360.metrics import ClassificationMetric
@@ -94,6 +89,7 @@ def _get_valid_fit_params(fit_method, params):
     # Retorna apenas os parâmetros que o fit() aceita
     return {k: v for k, v in params.items() if k in valid_params}
 
+
 # ===================== Model Training =====================
 
 def train_model(model_name, X_train, y_train, params_model, params_fit=None):
@@ -157,81 +153,82 @@ def apply_mitigation_post(y_pred, y_proba, y_test, A_test, mitigation_cfg):
 
 # ===================== Metrics =====================
 def evaluate_performance(y_true, y_pred, y_proba):
-    # Descobre automaticamente os rótulos únicos
-    unique_labels = pd.Series(y_true).dropna().unique()
-    if len(unique_labels) != 2:
-        raise ValueError(f"Target não binário: {unique_labels}")
 
-    # Cria mapeamento para 0 e 1
-    label_map = {unique_labels[0]: 0, unique_labels[1]: 1}
-    y_true_num = pd.Series(y_true).map(label_map).astype(int)
-    y_pred_num = pd.Series(y_pred).map(label_map).astype(int)
+    tn, fp, fn, tp = confusion_matrix(y_true, y_pred, labels=[0,1]).ravel()
+    accuracy    = (tp + tn) / (tp + tn + fp + fn) if (tp + tn + fp + fn) > 0 else 0.0
+    recall      = tp / (tp + fn) if (tp + fn) > 0 else 0.0
+    specificity = tn / (tn + fp) if (tn + fp) > 0 else 0.0
+    precision   = tp / (tp + fp) if (tp + fp)  > 0 else 0.0
+    npv         = tn / (tn + fn) if (tn + fn) > 0 else 0.0
+    f1_score    = (2 * precision * recall) / (precision + recall) if (precision + recall)  > 0 else 0.0
+    balancedaccuracy = (recall + specificity) / 2
 
-    tn, fp, fn, tp = confusion_matrix(y_true_num, y_pred_num, labels=[0,1]).ravel()
     return pd.DataFrame([{
-        "acuracia": accuracy_score(y_true_num, y_pred_num),
-        "precisao": precision_score(y_true_num, y_pred_num),
-        "npv": tn / (tn + fn) if (tn + fn) > 0 else 0.0,
-        "recall": recall_score(y_true_num, y_pred_num),
-        "specificity": tn / (tn + fp) if (tn + fp) > 0 else 0.0,
-        "f1-score": f1_score(y_true_num, y_pred_num),
-        "balanced_accuracy": balanced_accuracy_score(y_true_num, y_pred_num),
-        "roc_auc": roc_auc_score(y_true_num, y_proba),
-        "pr_auc": average_precision_score(y_true_num, y_proba),
-        "mcc": matthews_corrcoef(y_true_num, y_pred_num),
-        "log_loss": log_loss(y_true_num, y_proba),
         "true_positives": tp,
         "true_negatives": tn,
         "false_positives": fp,
-        "false_negatives": fn
+        "false_negatives": fn,
+        "accuracy": accuracy,
+        "recall": recall,        
+        "specificity": specificity,
+        "precision": precision,
+        "npv"     : npv,      
+        "f1-score": f1_score,
+        "balanced_accuracy": balancedaccuracy,
+        "roc_auc": roc_auc_score(y_true, y_proba),
+        "pr_auc": average_precision_score(y_true, y_proba),
+        "mcc": matthews_corrcoef(y_true, y_pred),
+        "log_loss": log_loss(y_true, y_proba)
     }])
 
 
-def evaluate_fairness(y_true, y_pred, A, protected_attribute, label):
-    # Converte y_true e y_pred para 0/1
-    unique_labels = pd.Series(y_true).dropna().unique()
-    if len(unique_labels) != 2:
-        raise ValueError(f"Target não binário: {unique_labels}")
-    label_map = {unique_labels[0]: 0, unique_labels[1]: 1}
-    y_true_num = pd.Series(y_true).map(label_map).astype(int)
-    y_pred_num = pd.Series(y_pred).map(label_map).astype(int)
-
-    # Converte atributo protegido para 0/1 (caso ainda não seja numérico)
-    A_num = pd.Series(A)
-    if not pd.api.types.is_numeric_dtype(A_num):
-        unique_attrs = A_num.dropna().unique()
-        if len(unique_attrs) != 2:
-            raise ValueError(f"Atributo protegido não binário: {unique_attrs}")
-        attr_map = {unique_attrs[0]: 0, unique_attrs[1]: 1}
-        A_num = A_num.map(attr_map).astype(int)
+def evaluate_fairness(y_true, y_pred, A, sensitive_attribute, target):
 
     dataset_true = BinaryLabelDataset(
-        df=pd.DataFrame({label: y_true_num, protected_attribute: A_num}),
-        label_names=[label],
-        protected_attribute_names=[protected_attribute],
+        df=pd.DataFrame({target: y_true, sensitive_attribute: A}),
+        label_names=[target],
+        protected_attribute_names=[sensitive_attribute],
         favorable_label=1,
         unfavorable_label=0
     )
 
     dataset_pred = dataset_true.copy()
-    dataset_pred.labels = y_pred_num.values.reshape(-1, 1)
+    dataset_pred.labels = y_pred
 
     metric = ClassificationMetric(
         dataset_true,
         dataset_pred,
-        privileged_groups=[{protected_attribute: 1}],
-        unprivileged_groups=[{protected_attribute: 0}]
+        privileged_groups=[{sensitive_attribute: 1}],
+        unprivileged_groups=[{sensitive_attribute: 0}]
     )
+
+    pg_value = True  # sempre True para privileged_groups
+    ug_value = False  # sempre False para unprivileged_groups
+
     return pd.DataFrame([{
         "statistical_parity_diff": metric.statistical_parity_difference(),
         "equalized_odds_diff": metric.equalized_odds_difference(),
         "average_odds_diff": metric.average_odds_difference(),
         "disparate_impact": metric.disparate_impact(),
-        "positive_predictive_value": metric.positive_predictive_value()
+        "generalized_entropy_index": metric.generalized_entropy_index(),
+        "accuracy_privileged": metric.accuracy(privileged=pg_value),
+        "accuracy_unprivileged": metric.accuracy(privileged=ug_value),
+        "selection_rate_privileged": metric.selection_rate(privileged=pg_value),
+        "selection_rate_unprivileged": metric.selection_rate(privileged=ug_value),
+        "false_positive_rate_privileged": metric.false_positive_rate(privileged=pg_value),
+        "false_positive_rate_unprivileged": metric.false_positive_rate(privileged=ug_value),
+        "false_negative_rate_privileged": metric.false_negative_rate(privileged=pg_value),
+        "false_negative_rate_unprivileged": metric.false_negative_rate(privileged=ug_value),
+        "ppv_privileged": metric.positive_predictive_value(privileged=pg_value),
+        "ppv_unprivileged": metric.positive_predictive_value(privileged=ug_value)
+        
     }])
+
+
 
 # ===================== Run Experiment =====================
 def run_experiment(config_path):
+
     config = load_config(config_path)
 
     # === Data ===
@@ -300,8 +297,8 @@ def run_experiment(config_path):
     # === Fairness Metrics ===
     fairness_metrics = evaluate_fairness(
         y_test, y_pred, A_test,
-        protected_attribute=config["dataset"]["sensitive"],
-        label=config["dataset"]["target"]
+        sensitive_attribute=config["dataset"]["sensitive"],
+        target=config["dataset"]["target"]
     )
 
     results = pd.concat([model_info, mitigation_info, performance_metrics, fairness_metrics], axis=1)
