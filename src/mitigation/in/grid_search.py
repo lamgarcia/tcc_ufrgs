d945d9ce01c02e76610015207b837916e70e2995
Luiz-Garcia-MSc <109922407+Luiz-Garcia-MSc@users.noreply.github.com>
2025-09-19 09:38:22 -0300

    SELECIONANDO PARAMETROS DO GRIDSEARCH

--- src/mitigation/in/grid_search.py
+++ src/mitigation/in/grid_search.py
@@ -16,6 +16,7 @@ def apply(model, X_train, y_train, A_train, mitigation_cfg):
     """
 
     from fairlearn.reductions import GridSearch, DemographicParity, EqualizedOdds
+    import inspect
 
     # Seleciona constraint
     constraint_type = mitigation_cfg.get("constraint", "DemographicParity")
@@ -25,12 +26,20 @@ def apply(model, X_train, y_train, A_train, mitigation_cfg):
         constraint = EqualizedOdds()
     else:
         raise ValueError(f"Constraint {constraint_type} não suportada.")
+    
+    # --- valida compatibilidade do modelo com o paramentor sample_weight do grid search---
+    if "sample_weight" not in inspect.signature(model.fit).parameters:
+        # Wrapper simples que ignora sample_weight
+        class Wrapper(model.__class__):
+            def fit(self, X, y, sample_weight=None, **kwargs):
+                return super().fit(X, y, **kwargs)
+        model = Wrapper(**model.get_params())
 
     # Cria mitigador usando o modelo original como estimador base
     mitigator = GridSearch(
         estimator=model,
         constraints=constraint,
-        grid_size=mitigation_cfg.get("grid_size", 10)
+        grid_size=mitigation_cfg.get("grid_size", 3)
     )
 
     # Ajusta o mitigador
