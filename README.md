a8fd7ab32ae807405bff4b95fef37cfdf360334a
Luiz-Garcia-MSc <109922407+Luiz-Garcia-MSc@users.noreply.github.com>
2025-12-19 18:18:09 -0300

    update

--- README.md
+++ README.md
@@ -1,8 +1,36 @@
 # TCC - Ufrgs
 
-#
+## \###Parametros dos experimentos gerais
+
+params.yaml
+
+## 
+
+## \### Gerar os arquivos individuais de experimentos a partir dos param.yaml
+
+python gera\_configs.py # serão salvos em \\configs\\
+
+## 
+
+## \###Rodas todos os epxerimentos em \\configs\\
+
+pyhton run\_all.py
 
 
 
-### No warnings
-set TF_ENABLE_ONEDNN_OPTS=0
+## \###rodar várias execuções de run\_all
+
+lote\_run\_all.bat
+
+# 
+
+## \###rodar apenas um experimento
+
+python run\_exp.py .\\configs\\adult\_\_bernoulli\_nb\_\_pre-none\_\_in-none\_\_post-none.yaml
+
+
+
+## \###No warnings
+
+set TF\_ENABLE\_ONEDNN\_OPTS=0
+
--- src/results/results_mean_std.py
+++ src/results/results_mean_std.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # === 1. Ler CSV ===
-df = pd.read_csv("runs_adult_30x_2.csv")
+df = pd.read_csv("runs_adult_30x_3.csv")
 
 # === 2. Chave única de agrupamento ===
 group_keys = ["model", "pre", "in", "post"]
@@ -12,8 +12,24 @@ df = df.drop(columns=["id", "data"], errors="ignore")
 # === 4. Identificar colunas que serão agregadas (todas exceto keys)
 agg_cols = [c for c in df.columns if c not in group_keys]
 
+print(df.groupby(group_keys).size())
+
 # === 5. Criar dicionário de agregação: mean + std
 agg_dict = {col: ["mean", "std"] for col in agg_cols}
+
+
+# 1. Verifique quantas linhas tem em cada grupo (tem que ser > 1, idealmente 30)
+print("Tamanho dos grupos (min, max):")
+sizes = df.groupby(group_keys).size()
+print(sizes.min(), sizes.max())
+
+# 2. Olhe a variação dentro de UM grupo específico
+exemplo_grupo = sizes.index[0] # Pega o primeiro grupo
+print(f"\nDados dentro do grupo {exemplo_grupo}:")
+subset = df.set_index(group_keys).loc[exemplo_grupo]
+print(subset.head(10)) # Veja se os valores das colunas são idênticos
+
+
 
 # === 6. Agrupar e calcular ===
 grouped = df.groupby(group_keys).agg(agg_dict)
@@ -40,6 +56,6 @@ final_cols = group_keys + metric_pairs
 grouped = grouped[final_cols]
 
 # === 10. Salvar CSV final ===
-grouped.to_csv("runs_adult_30x_2_mean_std.csv", index=False)
+grouped.to_csv("runs_adult_30x_teste_mean_std.csv", index=False)
 
 print("Arquivo gerado com sucesso!")
